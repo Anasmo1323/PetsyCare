d93c29bc0bcd4e672d8bb31005ad5fb891371fb0
Ali Zayan <alizayan684@gmail.com>
2025-12-03 22:58:03 +0200

    Add Secrets class for sensitive configuration

---
+++ petsy_care/lib/secrets.dart
@@ -0,0 +1,9 @@
+class Secrets {
+  // MUST match secrets.h EXACTLY
+  static const String aesKey = "petsycare_secret";
+  static const String aesIv = "petsycare_init_v";
+  
+  // Firebase user for device (same as ESP8266)
+  static const String deviceEmail = "cage001@petsy.com";
+  static const String devicePassword = "cage123456";
+}
